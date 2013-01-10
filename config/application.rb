443915031f4cf9b9a31469a43a0e50a0d40595eb
Sudhir Jonathan <sudhir.j@gmail.com>
2013-01-10 16:26:41 +0530

    added asset files to applicaiton.rb

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,8 @@ module Beacon
     # Enable the asset pipeline.
     config.assets.enabled = true
 
+    config.assets.precompile += %w( reader.js reader.css admin.css admin.js )
+
 
     # Version of your assets, change this if you want to expire all your assets.
     config.assets.version = '1.0'
