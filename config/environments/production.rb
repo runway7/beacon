8583ac79109d861049e899da165ede022cdcf515
Sudhir Jonathan <sudhir.j@gmail.com>
2013-01-10 16:30:41 +0530

    removed unnecesary setting from prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,14 +54,6 @@ Beacon::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
 
-  # Precompile additional assets.
-  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( reader.js reader.css admin.css admin.js )
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
